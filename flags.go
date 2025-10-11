dee97c5f471fa4f0d360bcd871e20248f9418461
Fyrna N. <imfyrna@gmail.com>
2025-10-11 22:01:07 +0700

    refactor: Paw* should be Flag sugar

--- flags.go
+++ flags.go
@@ -22,103 +22,76 @@ type Flag struct {
 	HelpText   string
 }
 
-// FlagBuilder provides a fluent interface for flag creation
-type FlagBuilder struct {
-	flag   *Flag
-	parser *Parser
-}
-
 // some litte sugar function :3
 
-// PawBool creates a new boolean flag builder
-func (p *Parser) PawBool(name string, aliases ...string) *FlagBuilder {
-	return &FlagBuilder{
-		flag: &Flag{
-			Name:     name,
-			Aliases:  aliases,
-			Type:     BoolType,
-			DefValue: false,
-		},
-		parser: p,
+// PawBool creates a new boolean flag
+func PawBool(name string, aliases ...string) *Flag {
+	return &Flag{
+		Name:     name,
+		Aliases:  aliases,
+		Type:     BoolType,
+		DefValue: false,
 	}
 }
 
-// PawString creates a new string flag builder
-func (p *Parser) PawString(name string, aliases ...string) *FlagBuilder {
-	return &FlagBuilder{
-		flag: &Flag{
-			Name:    name,
-			Aliases: aliases,
-			Type:    StringType,
-		},
-		parser: p,
+// PawString creates a new string flag
+func PawString(name string, aliases ...string) *Flag {
+	return &Flag{
+		Name:    name,
+		Aliases: aliases,
+		Type:    StringType,
 	}
 }
 
-// PawInt creates a new integer flag builder
-func (p *Parser) PawInt(name string, aliases ...string) *FlagBuilder {
-	return &FlagBuilder{
-		flag: &Flag{
-			Name:    name,
-			Aliases: aliases,
-			Type:    IntType,
-		},
-		parser: p,
+// PawInt creates a new integer flag
+func PawInt(name string, aliases ...string) *Flag {
+	return &Flag{
+		Name:    name,
+		Aliases: aliases,
+		Type:    IntType,
 	}
 }
 
-// PawUint creates a new unsigned integer flag builder
-func (p *Parser) PawUint(name string, aliases ...string) *FlagBuilder {
-	return &FlagBuilder{
-		flag: &Flag{
-			Name:    name,
-			Aliases: aliases,
-			Type:    UintType,
-		},
-		parser: p,
+// PawUint creates a new unsigned integer flag
+func PawUint(name string, aliases ...string) *Flag {
+	return &Flag{
+		Name:    name,
+		Aliases: aliases,
+		Type:    UintType,
 	}
 }
 
-// PawFloat creates a new float flag builder
-func (p *Parser) PawFloat(name string, aliases ...string) *FlagBuilder {
-	return &FlagBuilder{
-		flag: &Flag{
-			Name:    name,
-			Aliases: aliases,
-			Type:    FloatType,
-		},
-		parser: p,
+// PawFloat creates a new float flag
+func PawFloat(name string, aliases ...string) *Flag {
+	return &Flag{
+		Name:    name,
+		Aliases: aliases,
+		Type:    FloatType,
 	}
 }
 
-func (b *FlagBuilder) Default(value any) *FlagBuilder {
-	b.flag.DefValue = value
-	return b
+func (f *Flag) Default(value any) *Flag {
+	f.DefValue = value
+	return f
 }
 
-func (b *FlagBuilder) Required() *FlagBuilder {
-	b.flag.IsRequired = true
-	return b
+func (f *Flag) Required() *Flag {
+	f.IsRequired = true
+	return f
 }
 
-func (b *FlagBuilder) Help(text string) *FlagBuilder {
-	b.flag.HelpText = text
-	return b
+func (f *Flag) Help(text string) *Flag {
+	f.HelpText = text
+	return f
 }
 
-func (b *FlagBuilder) Choices(choices ...string) *FlagBuilder {
-	b.flag.ChoicesOpt = choices
-	return b
+func (f *Flag) Choices(choices ...string) *Flag {
+	f.ChoicesOpt = choices
+	return f
 }
 
-func (b *FlagBuilder) Range(min, max int) *FlagBuilder {
-	b.flag.Min = min
-	b.flag.Max = max
-	return b
-}
-
-// for chaining
-func (b *FlagBuilder) END() *Parser {
-	b.parser.AddFlags(b.flag)
-	return b.parser
+func (f *Flag) Range(min, max int) *Flag {
+	f.Min = min
+	f.Max = max
+	return f
 }
